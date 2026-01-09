d66a2ffffefdfb26079cc5da8670aebd8b22eb77
edslogia <esgonzalezc@udistrital.edu.co>
2026-01-09 11:59:47 -0500

    Add Customer module with CRUD operations and DTOs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MqttModule } from './mqtt/mqtt.module';
 import { OperatorModule } from './operator/operator.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [MqttModule, OperatorModule],
+  imports: [MqttModule, OperatorModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
