632520d5be8346177d2edc4e3d856bd9a9856ba8
edslogia <esgonzalezc@udistrital.edu.co>
2026-01-09 11:58:41 -0500

    Add Operator module with CRUD operations and DTOs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MqttModule } from './mqtt/mqtt.module';
+import { OperatorModule } from './operator/operator.module';
 
 @Module({
-  imports: [MqttModule],
+  imports: [MqttModule, OperatorModule],
   controllers: [AppController],
   providers: [AppService],
 })
